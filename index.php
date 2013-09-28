0852874781394d1949f032394272710c02e48907
IvayloYordanow <i.yordanow@gmail.com>
2013-09-28 22:28:43 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title></title>
+    </head>
+    <body>
+   <form name="Login" method="POST" action="files.php">
+       
+       <label for="user">Username</label>
+       <input type="text" name="username" id="user"/><br>
+       
+       <label for="pass">Password</label>
+       <input type="password" name="password" id="pass"/><br>
+       
+       <input type="submit" value="Submit">
+
+   </form>
+    </body>
+</html>
